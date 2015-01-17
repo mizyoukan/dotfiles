3267ac0572913619690953c20b7f42939d91d169
Tamaki Mizuha <mizyoukan@outlook.com>
2015-01-17 16:44:21 +0900

    Remove about window manager

--- setup.sh
+++ setup.sh
@@ -3,18 +3,9 @@
 cd $(dirname $0)
 CURDIR=`pwd`
 
-HOME_FILES=( .dmrc .gitconfig .gtkrc-2.0 .Xdefaults .xinitrc .xmobarrc .zshenv .zshrc )
+HOME_FILES=( .gitconfig .gtkrc-2.0 .xprofile .zshenv .zshrc )
 for file in ${HOME_FILES[@]}; do
   if [ ! -e $HOME/$file ]; then
     ln -s $CURDIR/$file $HOME/$file
   fi
 done
-
-if [ ! -e $HOME/.xprofile ]; then
-  ln -s $CURDIR/.xinitrc $HOME/.xprofile
-fi
-
-if [ ! -r $HOME/.xmonad/xmonad.hs ]; then
-  mkdir -p $HOME/.xmonad
-  ln -s $CURDIR/.xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
-fi
